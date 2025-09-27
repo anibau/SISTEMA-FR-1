6cf21b3dc830df3b577f4247ec007fae9fe8279e
Miriam Bautista Torres <miriam.bautista@kiwipay.pe>
2025-09-27 12:25:17 -0500

    delete  back

--- SISTEMA-FR-backend/src/productos/dto/update-producto.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType, OmitType } from '@nestjs/swagger';
-import { CreateProductoDto } from './create-producto.dto';
-
-export class UpdateProductoDto extends PartialType(
-  OmitType(CreateProductoDto, ['codigo'] as const)
-) {
-  historialPrecios?: { precio: number; fecha: Date; usuario: string; motivo?: string }[];
-}
-
